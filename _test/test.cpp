240a0f1499a48790107d667ecb027302328a1c7e
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-03-08 20:22:41 +0100

    Init

---
+++ _test/test.cpp
@@ -0,0 +1,14 @@
+// clang-format off
+#include <gtest/gtest.h> // must be first
+#include "__preprocessor__.h"
+
+TEST(SampleTest, Sanity)
+{
+    EXPECT_EQ(1, 1);
+}
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
