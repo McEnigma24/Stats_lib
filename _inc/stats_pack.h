d616f9ac210dd3140444426edc9d682af205c7a8
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-03-08 20:44:46 +0100

    quick

---
+++ _inc/stats_pack.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "__preprocessor__.h"
+#include <math.h>
+#include <vector>
+
+namespace Stats
+{
+
+    class stats_pack
+    {
+        u64 min;
+        u64 max;
+        u64 sum;
+        u64 count;
+        vector<u64> data;
+
+    public:
+        stats_pack();
+
+        void push(u64 p);
+        double calculate_relative_standard_deviation();
+        tuple<u64, u64, u64, double> get();
+        vector<u64>& get_data();
+    };
+
+} // namespace Stats
---
+++ _src/stats_pack.cpp
@@ -0,0 +1,47 @@
+#include "stats_pack.h"
+
+namespace Stats
+{
+
+    stats_pack::stats_pack()
+    {
+        min = -1; // max for uint
+        max = 0;
+        sum = 0;
+        count = 0;
+    }
+
+    void stats_pack::push(u64 p)
+    {
+        if (p < min) min = p;
+        if (max < p) max = p;
+
+        sum += p;
+        count++;
+
+        data.push_back(p);
+    }
+
+    double stats_pack::calculate_relative_standard_deviation()
+    {
+        double mean = ((double)sum) / ((double)count);
+        double sum_of_all_dev_square = 0.0;
+
+        for (auto& x : data)
+        {
+            sum_of_all_dev_square += pow2(x - mean);
+        }
+        sum_of_all_dev_square /= (double)count; // population
+
+        double standard_deviation = sqrt(sum_of_all_dev_square);
+
+        return 100 * (standard_deviation / mean);
+    }
+
+    tuple<u64, u64, u64, double> stats_pack::get()
+    {
+        return {min, static_cast<u64>(((double)sum) / ((double)count)), max, calculate_relative_standard_deviation()};
+    }
+
+    vector<u64>& stats_pack::get_data() { return data; }
+} // namespace Stats
